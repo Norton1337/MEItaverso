c3ab118c61c3eea2bf041c972c26b2a91cda473f
luisbusto <1190817@isep.ipp,pt>
2023-06-03 23:58:56 +0100

    Crate and Unalive

---
+++ Assets/Crate.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crate : MonoBehaviour {
+
+    public Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") &&
+        collision.gameObject.GetComponent<PlayerController>().isDead)
+        {
+            rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") &&
+        collision.gameObject.GetComponent<PlayerController>().isDead)
+        {
+            rb.constraints = RigidbodyConstraints2D.None;
+        }
+    }
+}
+
