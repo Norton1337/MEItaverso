44b389bfabb8ffe6e17f34c19878e6a34fd89be0
Paulo Norton <firewall.norton@gmail.com>
2023-06-03 22:17:02 +0100

    Level 2 unfinished logic
    NUNO BARBOSA IS THE GOAT

--- Assets/Scenes/Level2/level2_manager.cs
+++ Assets/Scenes/Level2/level2_manager.cs
@@ -2,25 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class level5_manager : MonoBehaviour
+public class level2_manager : MonoBehaviour
 {
     [SerializeField] private GameObject door;
     [SerializeField] private GameObject Lever;
+    [SerializeField] private GameObject Button;
 
     // Start is called before the first frame update
     void Start()
     {
         
     }
+
     // Update is called once per frame
     void Update()
     {
-        if(Lever.GetComponent<LeverTrigger>().isActive){
+        if(//Button.GetComponent<Button>().isPressed && 
+        Lever.GetComponent<LeverTrigger>().isActive){
            Destroy(door);
         }
 
-       // if(GetComponent<Button>().isPressed){
-           //TODO: MUDAR ESTADO DO PERSONAGEM PARA DEAD AF  E UNALIVE
-        //}
     }
 }
