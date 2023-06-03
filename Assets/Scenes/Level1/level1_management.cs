cb9554e3cf6ec1341bf41956fb4414f6600039af
luisbusto <1190817@isep.ipp,pt>
2023-06-04 00:30:22 +0100

    Game Over Screen

--- Assets/Scenes/Level1/level1_management.cs
+++ Assets/Scenes/Level1/level1_management.cs
@@ -10,21 +10,24 @@ public class level1_management : MonoBehaviour
 
     [SerializeField] private GameObject GameOverScreen;
 
+    public int ctdr = 0;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameOverScreen.SetActive(false);   
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Button.GetComponent<Button>().isPressed == true){
-            //TODO: FAKE GAME-OVER
+        if(Button.GetComponent<Button>().isPressed == true && ctdr < 1){
             GameOverScreen.SetActive(true);
-
+            StartCoroutine(PauseGame());
+            Debug.Log("Game Resumed After 3 Seconds End Screen");
+            ctdr++;
         }
 
         if( Button.GetComponent<Button>().isPressed && Lever.GetComponent<LeverTrigger>().isActive
@@ -32,5 +35,16 @@ public class level1_management : MonoBehaviour
             Debug.Log("Button is pressed and lever is active");
            Destroy(door);
         }
+    }
+
+    private IEnumerator PauseGame()
+    {
+        Time.timeScale = 0f;
+
+        yield return new WaitForSecondsRealtime(3f);
+
+        Time.timeScale = 1f;
+        GameOverScreen.SetActive(false);
+
     }
 }
